3f0472b069a8642bb00772de30d7e430240e0b10
Nicolai Cornelis <nickdnk@hotmail.com>
2024-10-26 23:41:15 +0200

    Respect skip_queue_declaration on NonExistentQueue error

--- sqsjobs/listener.go
+++ sqsjobs/listener.go
@@ -42,27 +42,29 @@ func (c *Driver) listen(ctx context.Context) { //nolint:gocognit
 				})
 
 				if err != nil { //nolint:nestif
-					var oErr *smithy.OperationError
-					if errors.As(err, &oErr) {
-						var rErr *http.ResponseError
-						if errors.As(oErr.Err, &rErr) {
-							var apiErr *smithy.GenericAPIError
-							if errors.As(rErr.Err, &apiErr) {
-								// in case of NonExistentQueue - recreate the queue
-								if apiErr.Code == NonExistentQueue {
-									c.log.Error("receive message", zap.String("error code", apiErr.ErrorCode()), zap.String("message", apiErr.ErrorMessage()), zap.String("error fault", apiErr.ErrorFault().String()))
-									_, err = c.client.CreateQueue(context.Background(), &sqs.CreateQueueInput{QueueName: c.queue, Attributes: c.attributes, Tags: c.tags})
-									if err != nil {
-										c.log.Error("create queue", zap.Error(err))
+					if !c.skipDeclare {
+						var oErr *smithy.OperationError
+						if errors.As(err, &oErr) {
+							var rErr *http.ResponseError
+							if errors.As(oErr.Err, &rErr) {
+								var apiErr *smithy.GenericAPIError
+								if errors.As(rErr.Err, &apiErr) {
+									// in case of NonExistentQueue - recreate the queue
+									if apiErr.Code == NonExistentQueue {
+										c.log.Error("receive message", zap.String("error code", apiErr.ErrorCode()), zap.String("message", apiErr.ErrorMessage()), zap.String("error fault", apiErr.ErrorFault().String()))
+										_, err = c.client.CreateQueue(context.Background(), &sqs.CreateQueueInput{QueueName: c.queue, Attributes: c.attributes, Tags: c.tags})
+										if err != nil {
+											c.log.Error("create queue", zap.Error(err))
+										}
+										// To successfully create a new queue, you must provide a
+										// queue name that adheres to the limits related to the queues
+										// (https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/limits-queues.html)
+										// and is unique within the scope of your queues. After you create a queue, you
+										// must wait at least one second after the queue is created to be able to use the <------------
+										// queue. To get the queue URL, use the GetQueueUrl action. GetQueueUrl require
+										time.Sleep(time.Second)
+										continue
 									}
-									// To successfully create a new queue, you must provide a
-									// queue name that adheres to the limits related to the queues
-									// (https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/limits-queues.html)
-									// and is unique within the scope of your queues. After you create a queue, you
-									// must wait at least one second after the queue is created to be able to use the <------------
-									// queue. To get the queue URL, use the GetQueueUrl action. GetQueueUrl require
-									time.Sleep(time.Second)
-									continue
 								}
 							}
 						}
